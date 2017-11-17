bd933f000911d88ae4d3049930e0896ffeb49786
Duarte Carvalho <up201503661@fe.up.pt>
2017-11-17 12:54:39 +0000

    Radioterapia ao código do @bernardomcbarbosa, 3 sesssões restantes

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -14,11 +14,14 @@ int main(int argc, char **argv)
   sef_startup();
   sys_enable_iop(SELF);
 
-	if (argc == 1) {					/* Prints usage of the program if no arguments are passed */
-		print_usage(argv);
-		return 0;
+	if (argc == 1)
+  {
+		print_usage(argv); //Prints usage of the program if no arguments are passed
+
+    return 0;
 	}
-	else return proc_args(argc, argv);
+	else
+    return proc_args(argc, argv);
 }
 
 static void print_usage(char **argv)
@@ -33,77 +36,120 @@ static void print_usage(char **argv)
 static int proc_args(int argc, char **argv)
 {
 	unsigned short tipo,tempo,led, *vetor,elementos=0;
-	int i;
 
-	
-	if (strncmp(argv[1], "kbscan_test", strlen("kbscan_test")) == 0) {
-		if (argc != 3) {
+  int i;
+
+	if (strncmp(argv[1], "kbscan_test", strlen("kbscan_test")) == 0)
+  {
+		if (argc != 3)
+    {
 			printf("kbd: wrong no. of arguments for kbd_test_scan()\n");
-			return 1;
+
+      return 1;
 		}
-		tipo = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
-		if (tipo == ULONG_MAX)
+
+    tipo = parse_ulong(argv[2], 10);						/* Parses string to unsigned long */
+
+    if (tipo == ULONG_MAX)
 			return 1;
-		printf("kbd::kbd_test_scan(%lu)\n", tipo);
-		return kbd_test_scan(tipo);
-	} 
-	else if (strncmp(argv[1], "kbscan_timer", strlen("kbscan_timer")) == 0) {
-		if (argc != 3) {
-			printf("kbd: wrong no. of arguments for kbd_test_timed_scan()\n");
-			return 1;
-		}
-		tempo = parse_ulong(argv[2], 10);						
-		if (tempo == ULONG_MAX)
-			return 1;
-		printf("kbd::kbd_test_timed_scan(%lu)\n", tempo);
-		return kbd_test_timed_scan(tempo);
+
+    printf("kbd::kbd_test_scan(%lu)\n", tipo);
+
+    return kbd_test_scan(tipo);
 	}
-	else if (strncmp(argv[1], "testar_leds", strlen("testar_leds")) == 0) {
-		if (argc < 3) {
-			printf("kbd: wrong no of arguments for kbd_test_leds()\n");
-			return 1;
+	else if (strncmp(argv[1], "kbscan_timer", strlen("kbscan_timer")) == 0)
+  {
+		if (argc != 3)
+    {
+			printf("kbd: wrong no. of arguments for kbd_test_timed_scan()\n");
+
+      return 1;
 		}
-		elementos = argc - 2; // Número de argumentos depois do diretório e do comando
-		vetor = (unsigned short*) malloc(elementos*sizeof(unsigned short)); //Alocar memória para o vetor
-		for(i=0;i< elementos;i++){
-			if (parse_ulong(argv[i+2],10) == ULONG_MAX) return 1;
-			else vetor[i] = parse_ulong(argv[i+2],10); }// Guardar no vetor os argumentos
-		
+
+    tempo = parse_ulong(argv[2], 10);
+
+    if (tempo == ULONG_MAX)
+			return 1;
+
+    printf("kbd::kbd_test_timed_scan(%lu)\n", tempo);
+
+    return kbd_test_timed_scan(tempo);
+	}
+	else if (strncmp(argv[1], "testar_leds", strlen("testar_leds")) == 0)
+  {
+		if (argc < 3)
+    {
+			printf("kbd: wrong no of arguments for kbd_test_leds()\n");
+
+      return 1;
+		}
+
+    elementos = argc - 2; // Número de argumentos depois do diretório e do comando
+
+    //Alocar memória para o vetor
+    vetor = (unsigned short*) malloc(elementos*sizeof(unsigned short));
+
+    for(i = 0; i < elementos; i++)
+    {
+			if(parse_ulong(argv[i+2], 10) == ULONG_MAX)
+        return 1;
+			else
+        vetor[i] = parse_ulong(argv[i+2], 10); // Guardar no vetor os argumentos
+    }
+
 		//Testar se todos os argumentos são 0,1 ou 2
-		for(i=0;i< elementos;i++){ if(vetor[i] < 0 || vetor[i] > 2){
-						printf("%d is not a valid LED!\n",vetor[i]);
-						return 1;}
-					}
-		
-		printf("kdb::kbd_test_leds(%d,'",elementos);	
-		for(i=0;i<elementos;i++){ if(i==elementos-1) printf("%u')\n",vetor[i]);
-					else printf("%u,",vetor[i]);}
-		
+		for(i = 0; i< elementos; i++)
+    {
+      if(vetor[i] < 0 || vetor[i] > 2)
+      {
+				printf("%d is not a valid LED!\n", vetor[i]);
+
+        return 1;
+      }
+		}
+
+		printf("kdb::kbd_test_leds(%d,'",elementos);
+
+		for(i = 0; i < elementos; i++)
+    {
+      if(i==elementos-1)
+        printf("%u')\n",vetor[i]);
+			else
+        printf("%u,",vetor[i]);
+    }
+
 		return kbd_test_leds(elementos,vetor);
 	}
-	else {
+	else
+  {
 		printf("kbd: %s - no valid function!\n", argv[1]);
-		return 1;
-	} 
+
+    return 1;
+	}
 }
 
 static unsigned long parse_ulong(char *str, int base)
 {
 	char *endptr;
-	unsigned long val;
+
+  unsigned long val;
 
 	/* Convert string to unsigned long */
-	val = strtoul(str, &endptr, base);			
+	val = strtoul(str, &endptr, base);
 
 	/* Check for conversion errors */
-	if ((errno == ERANGE && val == ULONG_MAX) || (errno != 0 && val == 0)) {
+	if ((errno == ERANGE && val == ULONG_MAX) || (errno != 0 && val == 0))
+  {
 		perror("strtoul");
-		return ULONG_MAX;
+
+    return ULONG_MAX;
 	}
 
-	if (endptr == str) {
+	if (endptr == str)
+  {
 		printf("timer: parse_ulong: no digits were found in %s\n", str);
-		return ULONG_MAX;
+
+    return ULONG_MAX;
 	}
 
 	/* Successful conversion */
--- lab3/test3.c
+++ lab3/test3.c
@@ -26,30 +26,39 @@ int hook_n = 10;
 
 //TIMER0 FUNCTIONS FROM OUR LAB2 //
 
-int timer_subscribe_int(void) {
+int timer_subscribe_int(void)
+{
 	/*	Subscribes and enables Timer 0 interrupts.
 	 Returns bit order in interrupt mask; negative value on failure */
 
-	int id = hook_n; // Guardar o valor de hook_n para retornar caso não haja falhas.
+	// Guardar o valor de hook_n para retornar caso não haja falhas.
+	int id = hook_n;
+
 	if (sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_n) != 0)
 		return -1;
+
 	if (sys_irqenable(&hook_n) != 0)
 		return -2; //No caso de sys_irqsetpolicy() não ligar
 
 	return id;
 }
 
-int timer_unsubscribe_int() {
-	// Unsubscribes Timer 0 interrupts. 
-	//Return 0 upon success and non-zero otherwise  
+int timer_unsubscribe_int()
+{
+	// Unsubscribes Timer 0 interrupts.
+	//Return 0 upon success and non-zero otherwise
+
 	if (sys_irqdisable(&hook_n) != 0)
 		return 1;
+
 	if (sys_irqrmpolicy(&hook_n) != 0)
 		return 2;
+
 	return 0;
 }
 
-void timer_int_handler() {
+void timer_int_handler()
+{
 	//Timer 0 interrupt handler
 	//Increments counter
 	contador++;
@@ -58,7 +67,8 @@ void timer_int_handler() {
 // ---------------------------------- //
 
 // KBD FUNCTIONS //
-int kbd_subscribe() {
+int kbd_subscribe()
+{
 	//Note that the keyboard's IRQ number is 1.
 
 	int id = hook_kbd; //Para nao perder o id original
@@ -72,8 +82,8 @@ int kbd_subscribe() {
 	return BIT(hook_kbd); //retorna o bit da interrupt
 }
 
-int kbd_unsubscribe() {
-
+int kbd_unsubscribe()
+{
 	int hook_kbd_temp = hook_kbd;
 
 	if (sys_irqdisable(&hook_kbd_temp) != OK)
@@ -85,14 +95,19 @@ int kbd_unsubscribe() {
 	return 0;
 }
 
-unsigned long ler_tecla() {
+unsigned long ler_tecla()
+{
 	unsigned long estado, tecla;
+
 	int tentativas = 0;
 
-	while (tentativas < 7) {
+	while (tentativas < 7)
+	{
 		if (sys_inb(0x64, &estado) != OK)
 			return -1; //Ler o status do Registo do KB
-		if (estado & BIT(0)) { //0 indica que o buffer do kb está vazio, 1 indica que o buffer tem informação mas ainda não foi lida
+		//0 indica que o buffer do kb está vazio, 1 indica que o buffer tem informação mas ainda não foi lida
+		if (estado & BIT(0))
+		{
 			if (sys_inb(0x60, &tecla) != OK)
 				return -1;
 			if ((estado & (PAR_ERR | TO_ERR)) == 0)
@@ -100,61 +115,89 @@ unsigned long ler_tecla() {
 			else
 				return -1;
 		}
+
 		tickdelay(micros_to_ticks(DELAY_US));
+
 		tentativas++;
 	}
 }
 
-int kbd_test_scan(unsigned short ass) { //Depending on whether its argument is zero or not, it should use the IH written in C or the IH written in assembly.
+int kbd_test_scan(unsigned short ass)
+{
+	//Depending on whether its argument is zero or not,
+	//it should use the IH written in C or the IH written in assembly.
 	// Need to subscribe the KBC interrupts
 	//read the scancode from the OUT_BUF
 	//the IH Must be disabled to prevent it from reading the OUT_BUF before your handler does it
-	// Use not only the IRQ_REENABLE but also the IRQ_EXCLUSIVE policy in sys_irqsetpolicy() , i.e. use IRQ_REENABLE|IRQ_EXCLUSIVE
+	// Use not only the IRQ_REENABLE but also the IRQ_EXCLUSIVE policy in sys_irqsetpolicy(),
+	// i.e. use IRQ_REENABLE|IRQ_EXCLUSIVE
 	//Usar driver_receive() loop
 	//Interrupt handler reads the bytes from the KBC’s OUT_BUF
 	//Só deve ler um byte de cada vez
 
 	int ipc_status;
+
 	message msg;
+
 	int r, byte = 0;
+
 	unsigned int irq_set;
+
 	unsigned long key, code;
 
 	irq_set = kbd_subscribe();
 
-	if (irq_set < 0) {
+	if (irq_set < 0)
+	{
 		printf("Erro ao ligar o interrupt\n");
+
 		return 1;
 	}
 
-	while (key != 0x81) { //Enquanto o code for diferente de ESC
+	while (key != 0x81) //Enquanto o code for diferente de ESC
+	{
 		/* Get a request message. */
-		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+		{
 			printf("driver_receive failed with: %d", r);
+
 			continue;
 		}
-		if (is_ipc_notify(ipc_status)) { /* received notification */
-			switch (_ENDPOINT_P(msg.m_source)) {
+
+		if (is_ipc_notify(ipc_status)) /* received notification */
+		{
+			switch (_ENDPOINT_P(msg.m_source))
+			{
 			case HARDWARE: /* hardware interrupt notification */
-				if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+				if (msg.NOTIFY_ARG & irq_set) /* subscribed interrupt */
+				{
 					if (!ass)
 						key = ler_tecla();
 					else
 						key = lerTeclaAss();
 
-					if (key == 0xe0 && byte == 0) { //Verificar se a tecla tem 2 ou 1 byte
+					if (key == 0xe0 && byte == 0)
+					{ //Verificar se a tecla tem 2 ou 1 byte
 						printf("Tecla com 2 bytes & ");
-						byte++;
-						code = 0;
-						code = key << 8;
 
-					} else {
+						byte++;
+
+						code = 0;
+
+						code = key << 8;
+					}
+					else
+					{
 						byte = 0;
+
 						code |= key;
+
 						if (key & M_or_B) //Verificar se a tecla é pressionada ou largada
 							printf("Breakcode -> 0x%02x\n", code);
+
 						else
 							printf("Makecode -> 0x%02x\n", code);
+
 						code = 0;
 					}
 				}
@@ -162,160 +205,240 @@ int kbd_test_scan(unsigned short ass) { //Depending on whether its argument is z
 			default:
 				break;
 			}/* no other notifications expected: do nothing */
-		} else { /* received a standard message, not a notification */
+		}
+		else
+		{
+			/* received a standard message, not a notification */
 			/* no standard messages expected: do nothing */
 		}
 	}
+
 	kbd_unsubscribe(); // Unsubscribes Keyboard Interrupts
+
 	printf("ESC\n");
+
 	return 0;
 }
 
-int escrever_kbc(unsigned char cmd) {
+int escrever_kbc(unsigned char cmd)
+{
 	unsigned long status;
 	/* loop while 8042 input buffer is not empty */
-	while (1) {
+
+	while (1)
+	{
 		if (sys_inb(STAT_REG, &status) != OK)
 			return 1; 	//Receber o Status do Keyboard
-		if ((status & BIT(1)) == 0) { //Se o Input Buffer Full estiver vazio
+		if ((status & BIT(1)) == 0) //Se o Input Buffer Full estiver vazio
+		{
 			if (sys_outb(OUT_BUF, cmd) != OK)
 				return 1;
 			else
 				return 0;
 		}
+
 		tickdelay(micros_to_ticks(DELAY_US));
 	}
+
 	return 1;
 }
 
-int escrever_comando(unsigned char cmd) {
+int escrever_comando(unsigned char cmd)
+{
 	unsigned long resposta = 0;
-//Enquanto a resposta não for ACK e escrever o comando no respetivo LED, repete o ciclo.
-	while (1) {
+	//Enquanto a resposta não for ACK e escrever o comando no respetivo LED, repete o ciclo.
+
+	while (1)
+	{
 		if (escrever_kbc(COMANDO_LED) != 0)
 			return 1; //Enviar o byte 0xED
 		if (sys_inb(OUT_BUF, &resposta) != OK)
 			return 1;
-		if (resposta == ACK) {
+		if (resposta == ACK)
+		{
 			escrever_kbc(cmd); //Enviar o byte correspondente ao led pretendido
+
 			return 0;
 		}
 	}
+
 	return 1;
 }
 
-int kbd_test_leds(unsigned short n, unsigned short *leds) {
+int kbd_test_leds(unsigned short n, unsigned short *leds)
+{
 	/*Cada elemento do vetor leds especifica o led que deve estar ligado*/
 
 	/*Valores aceitáveis: 0 1 2 */
 	// 0 - Scroll lock
 	// 1 - Numeric lock
 	// 2 - Caps lock
+
 	char locks[3][15];
+
 	strcpy(locks[0], "SCROLL LOCK");
+
 	strcpy(locks[1], "NUM LOCK");
+
 	strcpy(locks[2], "CAPS LOCK");
-	/*Temos de guardar o estado atual do Led para saber se o desligamos ou ligamos*/
-	/*usar a função do timer do lab2 para quando passar um segundo, em vez de imprimir os segundos que passaram, processar um dos valores do vetor */
+
+	/*Temos de guardar o estado atual do Led para saber se o desligamos ou ligamos
+	usar a função do timer do lab2 para quando passar um segundo,
+	em vez de imprimir os segundos que passaram, processar um dos valores do vetor */
+
 	int ipc_status;
+
 	message msg;
+
 	unsigned long byte;
+
 	int id;
+
 	int r;
+
 	unsigned int irq_set;
+
 	int i = 0; //Índice do vetor leds
+
 	id = timer_subscribe_int();
 
-	if (id < 0) {
+	if (id < 0)
+	{
 		printf("Erro ao ligar o interrupt\n");
+
 		return 1;
 	}
+
 	irq_set = BIT(id);
+
 	while (contador < n * 60) // o tempo depende do número de elementos do vetor leds
 	{
-		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+		{
 			printf("driver_receive failed with: %d", r);
+
 			continue;
 		}
-		if (is_ipc_notify(ipc_status)) { /* received notification */
-			switch (_ENDPOINT_P(msg.m_source)) {
+		if (is_ipc_notify(ipc_status)) /* received notification */
+		{
+			switch (_ENDPOINT_P(msg.m_source))
+			{
 			case HARDWARE: /* hardware interrupt notification */
-				if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+				if (msg.NOTIFY_ARG & irq_set) /* subscribed interrupt */
+				{
 					contador++; // Incrementa o contador
 
-					if ((contador % 60) == 0) {
+					if ((contador % 60) == 0)
+					{
 						//Processar os LED'S
 						byte = 0;
+
 						byte |= BIT(leds[i]);
+
 						if (escrever_comando(byte) != 0)
-							printf("Erro ao Ligar/Desligar o %s\n",
-									locks[leds[i]]);
+							printf("Erro ao Ligar/Desligar o %s\n", locks[leds[i]]);
 						else
-							printf("%s Ligado/Desligado com sucesso!\n",
-									locks[leds[i]]);
-						i++; //Incrementar o índice 
+							printf("%s Ligado/Desligado com sucesso!\n", locks[leds[i]]);
+
+						i++; //Incrementar o índice
 					}
 				}
 				break;
 			default:
 				break; /* no other notifications expected: do nothing */
 			}
-		} else { /* received a standard message, not a notification */
+		} else
+		{
+			/* received a standard message, not a notification */
 			/* no standard messages expected: do nothing */
 		}
 	}
+
 	timer_unsubscribe_int(); // Unsubscribes Timer 0 interrupts.
+
 	free(leds);
+
 	return 0;
 }
 
-int kbd_test_timed_scan(unsigned short n) {
-	/* should exit not only when the user releases the ESC key, but also if it does not receive a scancode for a number seconds equal to its argument, n. */
+int kbd_test_timed_scan(unsigned short n)
+{
+	/* should exit not only when the user releases the ESC key,
+	but also if it does not receive a scancode for a number seconds equal to its argument,
+	 n. */
 
 	int ipc_status;
+
 	message msg;
+
 	int r, byte = 0;
+
 	unsigned int irq_set_kbd, irq_set_timer;
+
 	unsigned long key, code;
+
 	int segundos = n;
 
 	irq_set_kbd = kbd_subscribe();
+
 	irq_set_timer = BIT(timer_subscribe_int());
 
-	if (irq_set_kbd < 0 || irq_set_timer < 0) {
+	if (irq_set_kbd < 0 || irq_set_timer < 0)
+	{
 		printf("Erro ao ligar o interrupt\n");
+
 		return 1;
 	}
 
-	while ((key != 0x81) && (contador < n * 60)) { //Enquanto o code for diferente de ESC e o tempo diferente de n segundos
+	while ((key != 0x81) && (contador < n * 60)) //Enquanto o code for diferente de ESC e o tempo diferente de n segundos
+	{
 		/* Get a request message. */
-		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+		if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0)
+		{
 			printf("driver_receive failed with: %d", r);
+
 			continue;
 		}
-		if (is_ipc_notify(ipc_status)) { /* received notification */
+		if (is_ipc_notify(ipc_status)) /* received notification */
+		{
 			if (msg.NOTIFY_ARG & irq_set_timer) //Notificacao do timer0
 				timer_int_handler(); //Incrementa contador
-			switch (_ENDPOINT_P(msg.m_source)) {
+
+			switch (_ENDPOINT_P(msg.m_source))
+			{
 			case HARDWARE: /* hardware interrupt notification */
-				if (msg.NOTIFY_ARG & irq_set_kbd) { /* subscribed interrupt */
+				if (msg.NOTIFY_ARG & irq_set_kbd) /* subscribed interrupt */
+				{
 					key = ler_tecla();
 
-					if (key == 0xe0 && byte == 0) { //Verificar se a tecla tem 2 ou 1 byte
+					if (key == 0xe0 && byte == 0)//Verificar se a tecla tem 2 ou 1 byte
+					{
 						printf("Tecla com 2 bytes & ");
+
 						byte++;
+
 						code = 0;
+
 						code = key << 8;
-					} else {
+					}
+					else
+					{
 						byte = 0;
+
 						code |= key;
+
 						if (key & M_or_B) //Verificar se a tecla é pressionada ou largada
 							printf("Breakcode -> 0x%02x\n", code);
-						else {
+
+						else
+						{
 							printf("Makecode -> 0x%02x\n", code);
+
 							segundos = n;
+
 							contador = 0;
 						}
+
 						code = 0;
 					}
 				}
@@ -323,11 +446,15 @@ int kbd_test_timed_scan(unsigned short n) {
 			default:
 				break;
 			}/* no other notifications expected: do nothing */
-			if (contador % 60 == 0 && contador != 0) {
+			if (contador % 60 == 0 && contador != 0)
+			{
 				printf("%ds\n", segundos);
+
 				segundos--;
 			}
-		} else { /* received a standard message, not a notification */
+		} else
+		{
+			/* received a standard message, not a notification */
 			/* no standard messages expected: do nothing */
 		}
 	}
@@ -335,7 +462,10 @@ int kbd_test_timed_scan(unsigned short n) {
 		printf("ESC -> Sair\n");
 	else
 		printf("0s -> Sair\n");
+
 	kbd_unsubscribe(); // Unsubscribes Keyboard Interrupts
+
 	timer_unsubscribe_int(); // Unsubscribes Timer0 Interrupts
+
 	return 0;
 }
